9d20b2947add6ee9fbb302255be3e5428da0e12e
dashuai <dashuai@maihaoche.com>
2016-12-06 11:30:12 +0800

    初始化工程

---
+++ IPhoneProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IPhoneProject
+//
+//  Created by dashuai on 16/12/6.
+//  Copyright © 2016年 mhc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IPhoneProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IPhoneProject
+//
+//  Created by dashuai on 16/12/6.
+//  Copyright © 2016年 mhc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
